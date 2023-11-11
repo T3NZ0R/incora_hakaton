0509cce3843146593f0c7874d94b82d282b0f4c4
Vitalii Shtark <t3nz000r@gmail.com>
2023-11-11 13:56:44 +0200

    feat: translation

---
+++ components/Footer.tsx
@@ -0,0 +1,10 @@
+import type { FC } from 'react'
+
+export const Footer: FC = () => {
+
+  return (
+    <footer>
+
+    </footer>
+  )
+}
--- next.config.js
+++ next.config.js
@@ -1,8 +1,21 @@
-/** @type {import('next').NextConfig} */
+// @ts-check
 const { i18n } = require('./next-i18next.config.js')
 
+// You can remove the following 2 lines when integrating our example.
+const { loadCustomBuildParams } = require('./next-utils.config')
+const { esmExternals = false, tsconfigPath } =
+  loadCustomBuildParams()
+
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-	i18n,
+  experimental: {
+    esmExternals
+  },
+  i18n,
+  reactStrictMode: true,
+  typescript: {
+    tsconfigPath
+  }
 }
 
 module.exports = nextConfig
