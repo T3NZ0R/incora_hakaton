c61ed719ed8fd90a1b4f0204e4be181f5f290d23
Vitalii Shtark <t3nz000r@gmail.com>
2023-11-11 16:55:20 +0200

    Revert "Header"

--- @types/resources.ts
+++
@@ -1,9 +0,0 @@
-import header from 'public/locales/uk/header.json';
-import footer from 'public/locales/uk/footer.json';
-
-const resources = {
-  header,
-  footer,
-} as const;
-
-export default resources;
